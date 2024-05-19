d047cacbf45bae9d301f59647ee92151c6afe7f9
Aksil Cherikh <aksil.cherikh@enseirb-matmeca.fr>
2024-05-20 01:54:33 +0200

    Added shadow to book boxes

--- client/src/LoginPage.jsx
+++ client/src/LoginPage.jsx
@@ -44,6 +44,7 @@ function LoginPage() {
 
             if (response) {
                 const data = await response.json();
+                console.log(data);
                 if (data.status === 'success') {
                     navigate('/library');
                 }
